12620db85eb472b06a4ec99cdb86c2b2229036b0
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2024-04-08 18:23:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,18 +3,21 @@ var isDate = function (input) {
 	if (input instanceof Date && !isNaN(input)) {
         return true;
     }
-
-    // Check if the input is a string that can be parsed into a valid date
+	if(input==='new Date()'){
+		return true;
+	}
+    
     if (typeof input === 'string') {
         const parsedDate = new Date(input);
         return !isNaN(parsedDate);
     }
+	
 
-    // Return false for other types of input
+
     return false;
 }
 ;
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
+//const input = prompt("Enter Date.");
 alert(isDate(input));
